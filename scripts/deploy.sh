13505037c6356095d126e83f7ff3f47f17fcb916
Christopher Geary <hello@crgeary.com>
2023-07-05 20:08:05 +0100

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,6 @@
 
 echo "🚀 Deploying [$APP_NAME] to [$APP_ENV]"
 
-echo "Docker: $DOCKER_TAG and $DOCKER_DIGEST"
+echo "Kubernetes: $K8S_NAMESPACE"
+echo "Docker (App): $DOCKER_TAG and $DOCKER_DIGEST"
+echo "Docker (Migrations): $DOCKER_MIGRATIONS_TAG and $DOCKER_MIGRATIONS_DIGEST"
